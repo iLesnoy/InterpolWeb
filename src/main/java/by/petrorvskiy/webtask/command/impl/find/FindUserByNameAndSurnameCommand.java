c9176b21b7e00d6712b0cc28594ff7b4359dc249
ilesnoy <Gamasileusbed@gmail.com>
2022-01-25 13:51:55 +0300

    add search by name&surname

--- src/main/java/by/petrorvskiy/webtask/command/impl/find/FindUserByNameAndSurnameCommand.java
+++ src/main/java/by/petrorvskiy/webtask/command/impl/find/FindUserByNameAndSurnameCommand.java
@@ -20,6 +20,7 @@ import static by.petrorvskiy.webtask.command.ParameterAndAttribute.LIST;
 public class FindUserByNameAndSurnameCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
     private final UserService userService = new UserServiceImpl();
+    private final String SPACE_REGEX = "\\s";
 
     @Override
     public Router execute(HttpServletRequest request) {
@@ -28,7 +29,7 @@ public class FindUserByNameAndSurnameCommand implements Command {
 
         String page = (String) session.getAttribute(ParameterAndAttribute.CURRENT_PAGE);
         String fullName = request.getParameter(ParameterAndAttribute.USER_FULLNAME);
-        List<String> nameAndSurname = Arrays.asList(fullName.split("\\s"));
+        List<String> nameAndSurname = Arrays.asList(fullName.split(SPACE_REGEX));
         List<User> users;
         try {
 
